b32389a9853eba679eda344d27f24802cecff075
Dammy <dammymoses2001@gmail.com>
2021-04-11 21:24:50 +0100

    updated

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import ThunkMiddleware from 'redux-thunk';
-import rootReducer from './reducers'
-const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware))
+import rootReducer from './reducers';
+const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware));
 
-export default store
+export default store;
